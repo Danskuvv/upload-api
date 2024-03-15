2d07ada212497b82737b863e1fe8b474a9868e11
Danskuvv <danielvv@metropolia.fi>
2024-03-15 21:17:32 +0200

    initial commit

---
+++ hybrid-types/MessageTypes.ts
@@ -0,0 +1,57 @@
+import { Like, MediaItem, UserWithNoPassword, Comment } from './DBTypes';
+
+type MessageResponse = {
+  message: string;
+};
+
+type ErrorResponse = MessageResponse & {
+  stack?: string;
+};
+
+type MediaResponse = MessageResponse & {
+  media: MediaItem | MediaItem[];
+};
+
+// for auth server
+type LoginResponse = MessageResponse & {
+  token: string;
+  message: string;
+  user: UserWithNoPassword;
+};
+
+type UserResponse = MessageResponse & {
+  user: UserWithNoPassword;
+};
+
+type UserDeleteResponse = MessageResponse & {
+  user: { user_id: number };
+};
+
+// for upload server
+type UploadResponse = MessageResponse & {
+  data: {
+    filename: string;
+    media_type: string;
+    filesize: number;
+  };
+};
+
+type LikeResponse = MessageResponse & {
+  like: Like;
+};
+
+type CommentResponse = MessageResponse & {
+  comment: Comment;
+};
+
+export type {
+  MessageResponse,
+  ErrorResponse,
+  MediaResponse,
+  LoginResponse,
+  UploadResponse,
+  UserResponse,
+  UserDeleteResponse,
+  LikeResponse,
+  CommentResponse
+};
---
+++ src/app.ts
@@ -0,0 +1,40 @@
+require('dotenv').config();
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import {notFound, errorHandler} from './middlewares';
+import api from './api';
+import {MessageResponse} from '@sharedTypes/MessageTypes';
+import path from 'path';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+app.use(cors());
+app.use(express.json());
+
+// WORK IN PROGRESS
+app.use(
+  '/api/v1/uploads',
+  express.static(path.join(__dirname, '..', 'uploads'))
+);
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: 'API location: api/v1',
+  });
+});
+
+app.use('/api/v1', api);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
